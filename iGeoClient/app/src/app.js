80399a5c9431efc76688dfcf88c0e35442076a00
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-06-26 20:45:56 -0300

    add route login e register form

--- iGeoClient/app/src/app.js
+++ iGeoClient/app/src/app.js
@@ -1,1 +1,11 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/login", {
+    	templateUrl : "../login.html",
+    	controller : "loginController"
+    }).when("/register", {
+        templateUrl: "../register.html",
+        controller: "registerController"
+    });
+});
---
+++ iGeoClient/app/src/controllers/loginController.js
@@ -0,0 +1,11 @@
+var app = angular.module("app");
+
+app.controller("loginController", function ($scope, $http) {
+    $scope.login = function (loginVO) {
+        $http.post("http://localhost:8080/iGeoWebServices/webresources/login", loginVO).then(function (response) {
+            console.log("Deu certo");
+        }), function (response) {
+            console.log("Não deu certo");
+        };
+    };
+});
