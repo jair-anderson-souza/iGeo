058728ff08b456f70dd242dcfb15cb87bf851e66
Anderson Souza <jair_anderson_bs@hotmail.com>
2017-06-26 20:47:53 -0300

    controller off

--- iGeoClient/app/src/app.js
+++ iGeoClient/app/src/app.js
@@ -2,8 +2,8 @@ var app = angular.module("app", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/login", {
-    	templateUrl : "../login.html",
-    	controller : "loginController"
+        templateUrl: "../login.html",
+        controller: "loginController"
     }).when("/register", {
         templateUrl: "../register.html",
         controller: "registerController"
